cc48d351599015af14e4ff9b2e4d0c03b8a97e00
Tim Schindler <xh3b4sd@gmail.com>
2017-02-23 17:46:15 +0100

    ensured types that make more sense

--- etcd/etcd.go
+++ etcd/etcd.go
@@ -7,6 +7,6 @@ import (
 type Etcd struct {
 	Domain string `json:"domain" yaml:"domain"`
 	IP     net.IP `json:"ip" yaml:"ip"`
-	Port   string `json:"port" yaml:"port"`
+	Port   int    `json:"port" yaml:"port"`
 	Prefix string `json:"prefix" yaml:"prefix"`
 }
