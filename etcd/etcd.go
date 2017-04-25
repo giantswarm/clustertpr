f99c37dcf8193975174753f4722a9091eb6242fe
Lorenzo Manacorda <lorenzo@mailbox.org>
2017-04-25 11:07:40 +0200

    fix keys with '_' not being loaded (#14)

--- etcd/etcd.go
+++ etcd/etcd.go
@@ -2,7 +2,7 @@ package etcd
 
 type Etcd struct {
 	// AltNames is the alternative names used to generate certificates for Etcd.
-	AltNames string `json:"alt_names" yaml:"altNames"`
+	AltNames string `json:"altNames" yaml:"altNames"`
 	// Domain is the API domain for etcd, e.g.
 	// etcd.<cluster-id>.g8s.fra-1.giantswarm.io.
 	Domain string `json:"domain" yaml:"domain"`
