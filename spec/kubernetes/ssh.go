78a16fb8b0f014d3bcbeda07e00cc3890b87cff8
Vaclav Rozsypalek <vaclav@giantswarm.io>
2017-09-12 18:39:59 +0200

    gofmt

--- spec/kubernetes/ssh.go
+++ spec/kubernetes/ssh.go
@@ -1,6 +1,7 @@
 package kubernetes
 
 import "github.com/giantswarm/clustertpr/spec/kubernetes/ssh"
+
 type SSH struct {
 	// PublicKeys is a list of SSH public keys being added to each Kubernetes
 	// node. It can contain admin specific public keys as well as customer
--- spec/kubernetes/ssh/user.go
+++ spec/kubernetes/ssh/user.go
@@ -1,6 +1,6 @@
 package ssh
 
 type User struct {
-	Name string `json:"name" yaml:"name"`
+	Name      string `json:"name" yaml:"name"`
 	PublicKey string `json:"publicKey" yaml:"publicKey"`
 }
